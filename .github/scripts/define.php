91b4f1a2ca513ae1e74da7c6cf9e1e09cc697bde
Matt Banner <git@matt.banner.sh>
2024-04-05 21:53:40 +0100

    Allow app directory

--- .github/scripts/define.php
+++ .github/scripts/define.php
@@ -1,42 +1,64 @@
 <?php
 
-const BASE_DIR = __DIR__.'/../../../';
+$define = new Define(
+    workingDir: $_ARGV[1] ?? null
+);
+$define();
 
-setOutput('php_version', getPhpVersion());
-setOutput('phpstan_level', (string) getPhpStanLevel());
-
-function setOutput(string $name, string $value): void
+class Define
 {
-    exec("echo $name=$value >> \$GITHUB_OUTPUT");
-}
+    private string $path;
 
-function getPhpVersion(): string
-{
-    $composer = json_decode(
-        file_get_contents(BASE_DIR.'/composer.json'),
-        true
-    );
+    public function __construct(
+        ?string $workingDir = null
+    )
+    {
+        $this->path = __DIR__.'/../../..';
 
-    if (! isset($composer['config']['platform']['php'])) {
-        echo "PHP version not set in composer.json\n";
-        exit(1);
+        if (! is_null($workingDir)) {
+            $this->path = sprintf('%s/%s', $this->path, $workingDir);
+        }
     }
 
-    return $composer['config']['platform']['php'];
-}
+    public function __invoke(): void
+    {
+        $this->setOutput('php_version', $this->getPhpVersion());
+        $this->setOutput('phpstan_level', (string) $this->getPhpStanLevel());
+    }
 
-function getPhpStanLevel(): int
-{
-    preg_match(
-        pattern: '/level:\s+(\d+)/i',
-        subject: file_get_contents(BASE_DIR.'/phpstan.neon'),
-        matches: $matches
-    );
+    private function setOutput(string $name, string $value): void
+    {
+        exec("echo $name=$value >> \$GITHUB_OUTPUT");
+    }
 
-    if (isset($matches[1])) {
-        return (int) $matches[1];
-    } else {
-        echo "PHPStan level not found in phpstan.neon\n";
-        exit(1);
+    private function getPhpVersion(): string
+    {
+        $composer = json_decode(
+            file_get_contents($this->path.'/composer.json'),
+            true
+        );
+
+        if (! isset($composer['config']['platform']['php'])) {
+            echo "PHP version not set in composer.json\n";
+            exit(1);
+        }
+
+        return $composer['config']['platform']['php'];
+    }
+
+    private function getPhpStanLevel(): int
+    {
+        preg_match(
+            pattern: '/level:\s+(\d+)/i',
+            subject: file_get_contents($this->path.'/phpstan.neon'),
+            matches: $matches
+        );
+
+        if (isset($matches[1])) {
+            return (int) $matches[1];
+        } else {
+            echo "PHPStan level not found in phpstan.neon\n";
+            exit(1);
+        }
     }
 }
